264e034921c51768c74bd3f808b03da087ccfb17
Laitas <marciukash@gmail.com>
2022-12-07 19:15:59 +0200

    img paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,9 +18,9 @@ const Header = () => {
 
   return (
     <Wrapper>
-      <img src="./luminor-giphy/TESTHY.svg" alt="" />
+      <img src="/luminor-giphy/TESTHY.svg" alt="" />
       <section>
-        <img src="./luminor-giphy/info.svg" alt="" />
+        <img src="/luminor-giphy/info.svg" alt="" />
         <p>
           Press <span>spacebar</span> to shuffle or{" "}
           <button onClick={shuffleImages}>Click here</button>
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -18,12 +18,12 @@ const Image = ({ img, idx }: Types) => {
 
       {img.locked ? (
         <span>
-          <img src="./luminor-giphy/unlocked.svg" alt="" />
+          <img src="/luminor-giphy/unlocked.svg" alt="" />
           Click to unlock
         </span>
       ) : (
         <span>
-          <img src="./luminor-giphy/locked.svg" alt="" />
+          <img src="/luminor-giphy/locked.svg" alt="" />
           Click to lock
         </span>
       )}
